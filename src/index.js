7b6e8e6fc6757ba6dcf2d5e82cf463b2b46d88b5
Den-08 <neryupov@rambler.ru>
2020-09-30 01:17:55 +0300

    init: Start & done task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let result;
+    if ((array) && (array.length > 0)) {
+        result = Math.min.apply(null, array);
+    } else {
+        result = 0;
+    }
+    return result;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let result;
+    if ((array) && (array.length > 0)) {
+        result = Math.max.apply(null, array);
+    } else {
+        result = 0;
+    }
+    return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+    let result;
+    if ((array) && (array.length > 0)) {
+        let summary = 0;
+        for (i = 0; i < array.length; i++) {
+            summary = summary + array[i];
+        }
+        result = summary / array.length;
+    } else {
+        result = 0;
+    }
+    return result;
 }
